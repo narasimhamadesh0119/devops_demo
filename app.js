d3c04e4f35b1474115a0e7d2031789908b6fe652
narasimhamadesh0119 <narasimhamadesh0119@gmail.com>
2026-05-12 13:14:16 +0530

    resolved conflicts and updated code

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express()
 
 app.get('/',(req,res)=> {
-    res.send("Hello world from devops pipeline , Simba here  ");
+    res.send("Hello world from devops pipeline , Simba here Vanakam da mapla NMIT la irundhu  ");
 });
 
 app.listen(7000,()=>{
