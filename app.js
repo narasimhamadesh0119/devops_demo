97e6073c1c9eebd6b7e7b13fd5e1b34381c4a1f5
narasimhamadesh0119 <narasimhamadesh0119@gmail.com>
2026-05-12 12:42:39 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express()
 
 app.get('/',(req,res)=> {
-    res.send("Hello world from devops pipeline ");
+    res.send("Hello world from devops pipeline , Simba here  ");
 });
 
 app.listen(7000,()=>{
