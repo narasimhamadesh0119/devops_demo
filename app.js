949dfeabec66943435e72c4fb28489e9d6ad3392
narasimhamadesh0119 <narasimhamadesh0119@gmail.com>
2026-05-19 10:23:17 +0530

    test NMIT

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express()
 
 app.get('/',(req,res)=> {
-    res.send("Hello world from devops pipeline, vyshanvi Doddaaa");
+    res.send("Hello world from devops pipeline, NMIT Office ");
 });
 
 app.listen(7000,()=>{
