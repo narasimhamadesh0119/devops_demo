ad2ce8535a466b4f92a2f1f03f095582b71c4404
narasimhamadesh0119 <narasimhamadesh0119@gmail.com>
2026-05-19 11:13:28 +0530

    fixed k8s access 1

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express()
 
 app.get('/',(req,res)=> {
-    res.send("Hello world from devops pipeline, checking for kubernetes Cluster");
+    res.send("Hello world from devops pipeline, checking for kubernetes Clusters");
 });
 
 app.listen(7000,()=>{
