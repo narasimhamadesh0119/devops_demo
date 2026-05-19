2e91b6e71cca515959eed48b4afbaeed6e6c71ac
narasimhamadesh0119 <narasimhamadesh0119@gmail.com>
2026-05-19 11:18:16 +0530

    fixed kubeconfig crted config

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express()
 
 app.get('/',(req,res)=> {
-    res.send("Hello world from devops pipeline, checking for kubernetes Clusters");
+    res.send("Hello world from devops pipeline, checking for kubernetes Clusters with correct config");
 });
 
 app.listen(7000,()=>{
